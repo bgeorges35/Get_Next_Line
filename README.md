e70e535f890f5e9b2051afc13668c5eb91d2a480
Benoit GEORGES <bgeorges@e1r2p2.42.fr>
2018-05-26 18:49:34 +0200

    Project finish

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Get_Next_Line
 School project @42
+
+` int	get_next_line(const int fd, char **line); `
+
+The goal of this project was to write a function to read and return the next line.
---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgeorges <bgeorges@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/08 17:41:36 by bgeorges          #+#    #+#             */
+/*   Updated: 2018/02/16 11:47:51 by bgeorges         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include "libft/libft.h"
+# define BUFF_SIZE 1024
+
+int		get_next_line(const int fd, char **line);
+#endif
